dfba6b5a89a28eaad89772d7e5274304c70ccf57
Alexey <20alexey02@gmail.com>
2026-02-12 09:12:51 +0000

    day 2 of fish 7

--- server.js
+++ server.js
@@ -9,3 +9,23 @@ let tasks = [];
 app.get("/tasks", (req, res) => {
     res.json(tasks);
 });
+
+//cors
+const express = require('express');
+app.use(cors());
+
+//post new task
+app.post("/tasks", (req, res) => {
+    const task = req.body;
+    tasks.push(task);
+    res.status(201).json(task);
+});
+
+//delete task 
+app.delete("/tasks/:id", (req, res) => {
+    const id = req.params.id;
+    tasks = tasks.filter(task => task.id !== id);
+    res.json({ message: "Task deleted" });
+});
+
+app.listen(3000, () => console.log("API running "));
